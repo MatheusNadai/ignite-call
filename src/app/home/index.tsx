54140aab90153583e8a0c54e5e24b850b71dfcf8
Matheus de Nadai <matheus.nadai@hotmail.com>
2023-05-22 16:06:25 -0300

    first commit

---
+++ src/app/home/index.tsx
@@ -0,0 +1,28 @@
+/* eslint-disable jsx-a11y/alt-text */
+"use client";
+import { Heading, Text } from "@ignite-ui/react";
+import { Container, Hero, Preview } from "./styles";
+import previewImage from "../../assets/image_calendar.png";
+import Image from "next/image";
+export default function Home() {
+  return (
+    <Container>
+      <Hero>
+        <Heading size="4xl">Agendamento Descomplicado</Heading>
+        <Text size="lg">
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
+        </Text>
+      </Hero>
+      <Preview>
+        <Image
+          src={previewImage}
+          alt="calendario"
+          height={400}
+          // quality={100}
+          priority
+        />
+      </Preview>
+    </Container>
+  );
+}
